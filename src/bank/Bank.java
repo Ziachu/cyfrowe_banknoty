479d7b25663f11b337a3ba0b84333d1164619189
Michał Ziach <ziachmichal@gmail.com>
2015-11-12 21:34:47 +0100

    ADD simple client-server connections

---
+++ src/bank/Bank.java
@@ -0,0 +1,78 @@
+package bank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import vendor.Command;
+import vendor.Pair;
+
+public class Bank {
+
+	public static void main(String[] args) throws IOException {
+		int port = 4444;
+		String server_address = "localhost";
+		
+		Socket socket = new Socket(server_address, port);
+		
+		Scanner user_in = new Scanner(System.in);
+		BufferedReader socket_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter socket_out = new PrintWriter(socket.getOutputStream(), true);
+		
+		String user_input, user_role;
+		Command cmd;
+		
+		while (true) {
+			user_input = user_in.nextLine();
+			
+			try {
+				cmd = Command.valueOf(user_input);
+				socket_out.println(cmd);
+			} catch (NullPointerException e) {
+				log("Wrong command.", true);
+				continue;
+			}
+			
+			switch(cmd) {
+			case role:
+				log("Enter your role: ", false);
+				user_role = user_in.nextLine();
+				
+				socket_out.println(user_role);
+				
+				break;
+			case usr:
+				int no_users = Integer.parseInt(socket_in.readLine());
+				
+				log("(" + Integer.toString(no_users) + ")", false);
+				for (int i = 0; i < no_users; i++) {
+					log(socket_in.readLine(), false);
+				}
+				
+				
+				break;
+			case exit:
+				log("Shutting down the connection.", true);
+				socket.close();
+				user_in.close();
+				System.exit(-1);
+				
+				break;
+			default:
+				log("Wrong command, try again.", true);	
+				break;
+			}
+		}
+	}
+	
+	private static void log(String msg, boolean new_line) {
+		if (new_line)
+			System.out.println(">>> " + msg);
+		else
+			System.out.print(msg + " ");
+	}
+
+}
---
+++ src/server/Server.java
@@ -0,0 +1,118 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+import vendor.Command;
+import vendor.Pair;
+
+public class Server {
+	private static int port = 4444;
+	private static HashMap<String, Pair<BufferedReader, PrintWriter>> users;
+
+	// tworzy listener'a i odrębne wątki do obsługi każdego użytkownika
+	public static void main(String[] args) throws IOException {
+		ServerSocket listener = new ServerSocket(port);
+		users = new HashMap<String, Pair<BufferedReader, PrintWriter>>();
+
+		System.out.println("Listening at: " + listener.getInetAddress());
+
+		try {
+			while (true) {
+				new BackgroundServer(listener.accept()).start();
+			}
+		} finally {
+			listener.close();
+		}
+	}
+
+	private static class BackgroundServer extends Thread {
+		private Socket socket;
+		private BufferedReader socket_in;
+		private PrintWriter socket_out;
+		private String user_role;
+		private Pair<BufferedReader, PrintWriter> io_pair;
+		
+		BackgroundServer(Socket socket) {
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+			try {
+				log("Connection established with " + socket.getInetAddress() + ".");
+
+				socket_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				socket_out = new PrintWriter(socket.getOutputStream(), true);
+				io_pair = new Pair<BufferedReader, PrintWriter>(socket_in, socket_out);
+
+				String user_input;
+				Command cmd;
+
+				while (!socket.isClosed()) {
+					user_input = socket_in.readLine();
+					cmd = Command.valueOf(user_input);
+
+					switch (cmd) {
+					// logowanie użytkownika (przypisanie użytkownikowi roli)
+					case role:
+						user_role = socket_in.readLine();
+
+						users.put(user_role, io_pair);
+
+						log(user_role + " logged in.");
+
+						break;
+					// wylistowanie użytkownikami wszystkich zalogowanych
+					case usr:
+						socket_out.println(users.size());
+
+						for (Map.Entry<String, Pair<BufferedReader, PrintWriter>> user : users.entrySet()) {
+							socket_out.println(user.getKey());
+						}
+
+						break;
+					case exit:
+						users.remove(user_role);
+						
+						if (users.containsKey(user_role)) {
+							log("ERROR.005: " + user_role + " still logged in; server run() switch case statement.");
+						} else {
+							log(user_role + " logged out.");
+							socket.close();
+						}
+						
+						break;
+					default:
+						socket.close();
+
+						break;
+					}
+				}
+
+			} catch (IOException e) {
+				log("ERROR.001: in&out problem; BackgroundServer run() method.");
+				e.printStackTrace();
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					log("ERROR.002: couldn't close socket; BackgroundServer run() method.");
+				}
+
+				log("Connection with " + socket.getInetAddress()
+						+ " shut down.");
+			}
+		}
+
+		private void log(String msg) {
+			System.out.println(msg);
+		}
+	}
+}
