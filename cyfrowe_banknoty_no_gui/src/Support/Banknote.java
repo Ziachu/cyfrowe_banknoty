30dee8936520cabd41d9be9af131626a2451c6fb
damdud <damdud@st.amu.edu.pl>
2015-12-03 15:08:26 +0100

    ADD Banknote.java

---
+++ cyfrowe_banknoty_no_gui/src/Support/Banknote.java
@@ -0,0 +1,10 @@
+package Support;
+
+/**
+ TODO: musi miec miejsca na IDBanknotu, Kwotę, 4 listy T-W, S-U po 100 obiektow typu series
+ Generate_banknote(Y-to podaje ALICE, Xi-losowe(z series), I-liste 100 obiektow typu series(identyfikujacych) - otrzymuje po 1 elemencie
+ z listy i sobie zczytuje do swojej listy te 100 banknotow w forze
+ ALICE PODAJE Y i CIAGI IDENTYFIKUJACE(sama sobie je generuje z series zeby miec swoj komplet)
+ */
+public class Banknote {
+}
