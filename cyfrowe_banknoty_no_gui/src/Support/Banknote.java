2477a6e208b6f970711c4d58c2655bf6fd14af41
damdud <damdud@st.amu.edu.pl>
2015-12-04 13:04:38 +0100

    EDIT: Banknote.java

--- cyfrowe_banknoty_no_gui/src/Support/Banknote.java
+++ cyfrowe_banknoty_no_gui/src/Support/Banknote.java
@@ -7,4 +7,32 @@ package Support;
  ALICE PODAJE Y i CIAGI IDENTYFIKUJACE(sama sobie je generuje z series zeby miec swoj komplet)
  */
 public class Banknote {
+    private static int amount;
+    private static int banknote_id;
+    private static Series s_series;
+    private static Series u_series;
+    private static Series t_series;
+    private static Series w_series;
+
+    public Banknote () {}
+
+    public Banknote (int amount, int banknote_id, Series s_series, Series u_series, Series t_series, Series w_series) {
+        this.amount = amount;
+        this.banknote_id = banknote_id;
+        this.s_series = s_series;
+        this.u_series = u_series;
+        this.t_series = t_series;
+        this.w_series = w_series;
+
+    }
+
+    public void generateBanknote(int amount, int banknote_id, Series s_series, Series u_series, Series t_series, Series w_series){
+        //todo
+    }
+
+
+
+
+
+
 }
